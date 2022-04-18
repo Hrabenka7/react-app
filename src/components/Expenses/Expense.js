9a4753bffe14152450fed4c9b86f279d322ea8d8
Hrabenka7 <markettka@email.cz>
2022-04-18 13:11:42 +0200

    add useState

--- src/components/Expenses/Expense.js
+++ src/components/Expenses/Expense.js
@@ -20,9 +20,9 @@ const Expense = (props) => {
           date={props.expenses[2].date}
         ></ExpenseItem>
           <ExpenseItem
-          title={props.expenses[2].title}
-          amount={props.expenses[2].amount}
-          date={props.expenses[2].date}
+          title={props.expenses[3].title}
+          amount={props.expenses[3].amount}
+          date={props.expenses[3].date}
         ></ExpenseItem>
     </Card>
   );
